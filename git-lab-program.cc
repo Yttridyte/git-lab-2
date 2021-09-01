1e85e2c02bb67588c367506a36224dcd8e94f823
Yttridyte <jl205118@ohio.edu>
2021-09-01 17:07:55 -0400

    GitHub Upload

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Jack Livingston>
+ *        Date: <09/01/21>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) 
+{
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+    
+}// main
